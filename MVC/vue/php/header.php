a000f1a6e93bf6c6b37f2b018982bbdeb9716ae8
Tsunehito Higa <tsunehiga0310@gmail.com>
2018-07-12 23:09:04 +0200

    changé le couleur de fond

--- MVC/vue/php/header.php
+++ MVC/vue/php/header.php
@@ -1,9 +1,9 @@
 <header id="header" class="jumbotron">
     <section id="entete" class="row">
-        <div id="logo" class="col-md-3 col-lg-2">
+        <div id="logo" class="col-sm-offset-4 col-sm-4 col-lg-2">
             <img src="../../../img/logo_pta.png" alt="LOGO PTA" class="rounded">
         </div>
-        <div id="titre" class="col-md-offset-3 col-md-6 col-lg-offset-2 col-lg-8">
+        <div id="titre" class="col-md-offset-6 col-md-6 col-lg-offset-2 col-lg-8">
             <h2>PTA Tour Planner</h2>
         </div>
     </section>
