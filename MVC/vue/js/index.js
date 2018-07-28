2815c03859450aa9bd72e60321064e88a076453e
Tsunehito Higa <tsunehiga0310@gmail.com>
2018-07-28 16:41:06 +0200

    créé nouveau fichiers

---
+++ MVC/vue/js/index.js
@@ -0,0 +1,8 @@
+// fonction pour le navPills
+// $(document).ready(function()){
+//     var hashTabName = document.location.hash;
+//     if(hashTabName){
+//         $('.nav-pills a[href=' + hashTabName +  ']').tab('show');
+//     }
+// };
+
---
+++ MVC/vue/php/message.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link href="https://stackpath.bootstrapcdn.com/bootswatch/4.1.1/minty/bootstrap.min.css" rel="stylesheet" integrity="sha384-4eGtnTOp6je5m6l1Zcp2WUGR9Y7kJZuAiD3Pk2GAW3uNRgHQSIqcrcAxBipzlbWP" crossorigin="anonymous">
+    <title>Vous souhaitez créer votre propre compte?</title>
+</head>
+<body class="container">
+    <div class="align-self-center">
+        <div class="card border-secondary mb-3" style="max-width: 30rem;">
+            <div class="card-header">Vous souhaitez créer un compte?<br>Ou vous oubliez votre mot de passe?</div>
+            <div class="card-body">
+                <h4 class="card-title">Contactez-nous!</h4>
+                <divclass="card-text">
+                    <p>ADDRESS :  ​8 rue Sainte Anne 75001 Paris, ​FRANCE</p>
+                    <p>TEL :  +33 (0)1 4296 1881</p>
+                    <p>FAX :  +33 (0)1 4296 1209</p>
+                    <p>​営業時間 月～金曜日の09:00～18:00 (フランス時間)</p>
+                    <p>※土・日曜日及びフランスの祝祭日は休業</p>
+                </div>
+                <div class="row justify-content-center">
+                    <a type="button" class="btn btn-outline-secondary" href="login.php">Retour</a>
+                </div>
+            </div>
+        </div>
+    </div>
+</body>
+</html>
